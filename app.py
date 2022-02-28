4236d9a7c73258de728eb047865d292f6185f99e
Stenly19 <komanglangendria04@gmail.com>
2022-03-01 00:19:39 +0800

    feat: membuat halaman kirim surat

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, redirect, render_template, request, session
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    if not session.get("user_id"):
+        return redirect("/kirim")
+
+    return redirect("/inbox")
+
+
+@app.route("/kirim", methods=["GET", "POST"])
+def kirim():
+    if request.method == "GET":
+        return render_template("kirim.html")
+
+    # TODO: Menangani request via POST
+
+
+@app.route("/inbox")
+def inbox():
+    return "TODO"
