4eaaeaa662fca166582888e77c1921a7a210965a
Stenly19 <komanglangendria04@gmail.com>
2022-03-04 16:02:20 +0800

    fix: memperbaiki url yang salah

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def perlu_masuk(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if session.get("id_pengguna") is None:
-            return redirect(url_for("login", next=request.url))
+            return redirect(url_for("masuk", next=request.url))
         return f(*args, **kwargs)
 
     return decorated_function
